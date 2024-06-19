f251734861968f3728f9f1a4fc0a96f80f6c149f
Uwais Jawed <uwaisjawed8081@gmail.com>
2024-06-19 23:10:22 +0530

    Updated base url

--- frontend/notes-app/vite.config.js
+++ frontend/notes-app/vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Ensure this is set to '/' for root domain hosting
-})
+  //use dotenv in Vite
+  base: "/", // Ensure this is set to '/' for root domain hosting
+});
